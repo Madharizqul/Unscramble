453312751ad2c761d6c3b7de4e5247304e2e2242
madharizqul76 <madharizkul@gmail.com>
2023-09-19 08:57:43 +0700

    Pertemuan_1

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/ListofWords.kt
@@ -0,0 +1,12 @@
+package com.example.android.unscramble.ui.game
+
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 20
+
+// List with all the words for the Game
+val allWordsList: List<String> =
+    listOf("Name",
+        "Anda", "Adit", "Alpan", "Ajez", "Bagas", "Bayu", "Toni", "Rani", "Hanum", "Isti",
+        "Kara", "MAda", "Martis", "Melani", "Niken", "Nuha", "Rahma", "Raka", "Resky",
+        "Rico", "Ridzwan", "Ruh", "Ryan", "Sapiq", "Wahyu","Dinda")
